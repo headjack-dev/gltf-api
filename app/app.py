147db4a391c0b461281791273c840cd026074f31
Nick Kraakman <nickkraakman@gmail.com>
2017-11-27 14:20:17 +0000

    Setup basic project structure and add requirements file

---
+++ app/app.py
@@ -0,0 +1,18 @@
+import web
+# See http://webpy.org/docs/0.3/tutorial
+
+urls = (
+	'/', 'index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class index:
+	def GET(self):
+		greeting = "Hello World"
+		return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+	app.run()
