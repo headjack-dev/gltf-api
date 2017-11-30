a58dea89f6b556f363bb1aec90451b97215a5ee4
Nick Kraakman <nick@purplepill.io>
2017-11-30 13:22:49 +0100

    Improve code comments and error handling

--- app/api.py
+++ app/api.py
@@ -1,65 +1,92 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request  # redirect, url_for
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from flask.ext.jsonpify import jsonify
 from werkzeug.utils import secure_filename
 import os
 
-# Config
+
+# CONFIG
+
+UPLOAD_FOLDER = '../static/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '../static/uploads' # Upload folder
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 api = Api(app)
 
-# Check if uploaded file extension is allowed
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Define API endpoints
+# FUNCTIONS
+
+def allowed_file(filename):
+    """Check if uploaded file extension is allowed.
+
+    Args:
+        filename (str): Filename to check.
+
+    Returns:
+        bool: True if file extension is allowed, False otherwise.
+    """
+    result = '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return result
+
+
+# API ENDPOINTS
+
 class Employees(Resource):
+
     def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        conn = db_connect.connect()  # connect to database
+        query = conn.execute("select * from employees")  # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
+
 
 class Tracks(Resource):
+
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
+
 
 class Employees_Name(Resource):
+
     def get(self, employee_id):
         conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
 
-class UploadFile(Resource):
-    #def post(self, fname):
+
+class Uploads(Resource):
+
     def post(self):
         file = request.files['file']
-        print(file)
-        if file and allowed_file(file.filename):
-            # From flask uploading tutorial: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #TODO:
-            #return redirect(url_for('uploaded_file', filename=filename))
-            result = {'result': 'Success'}
-            return jsonify(result)
-        else:
-            # return error
-            result = {'result': 'Failure'}
-            return jsonify(result)
+        if not file:
+            error = {'error': 'No file present in the request'}
+            return jsonify(error)
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
-api.add_resource(UploadFile, '/upload') # Route 4
+        filename = secure_filename(file.filename)
+        if not allowed_file(filename):
+            extension = file.filename.rsplit('.', 1)[1].lower()
+            error = {'error': 'The %s extension is not allowed, please upload an fbx, obj, or zip file' % extension}
+            return jsonify(error)
+
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        result = {'result': 'Successfully uploaded %s' % filename}
+        return jsonify(result)
+        # return redirect(url_for('uploaded_file', filename=filename)) # In case we want to display a webpage
+
+
+# ROUTES
+
+api.add_resource(Employees, '/v1/employees')  # Route_1
+api.add_resource(Tracks, '/v1/tracks')  # Route_2
+api.add_resource(Employees_Name, '/v1/employees/<employee_id>')  # Route_3
+api.add_resource(Uploads, '/v1/uploads')  # Route 4
 
 if __name__ == '__main__':
      app.run(port='5010')
--- test/test-api.py
+++ test/test-api.py
@@ -3,9 +3,9 @@ import requests
 
 # Upload a file
 print("Upload started")
-url = 'http://127.0.0.1:5010/upload' # API endpoint
+url = 'http://127.0.0.1:5010/v1/uploads' # API endpoint
 filename = 'test.png'
 file = open(filename, 'rb') # File to upload
-r = requests.post(url=url, data={'title': filename}, files={'file': file})
+r = requests.post(url=url, data={'uploaded_file': filename}, files={'file': file})
 print(r.status_code)
 print(r.text)
