bcd398bdb84bf61597bbfa12706a08077baf4868
Nick Kraakman <nick@purplepill.io>
2017-12-02 10:46:30 +0100

    Rewrite tests to use try / except

--- tests/test-api.py
+++ tests/test-api.py
@@ -7,16 +7,23 @@ print("Upload unsupported file")
 url = 'http://127.0.0.1:5011/v1/models'  # API endpoint
 filename = 'test.png'
 file = open(filename, 'rb')  # File to upload
-r = requests.post(url=url, data={'uploaded_file': filename}, files={'file': file})
+try:
+    r = requests.post(url=url, data={'uploaded_file': filename}, files={'file': file})
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print(e)
 print(r.status_code)
 print(r.text)
+
 
 # Upload a supported file
 print("Upload supported file")
 url = 'http://127.0.0.1:5011/v1/models'  # API endpoint
 filename = 'test.fbx'
 file = open(filename, 'rb')  # File to upload
-r = requests.post(url=url, data={'uploaded_file': filename}, files={'file': file})
+try:
+    r = requests.post(url=url, data={'uploaded_file': filename}, files={'file': file})
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print(e)
 print(r.status_code)
 print(r.text)
 
@@ -25,7 +32,10 @@ print(r.text)
 print("Post source_path to supported file")
 url = 'http://127.0.0.1:5011/v1/models'  # API endpoint
 source_path = 'https://purplepill.io/wp-includes/3d/test.FBX'
-r = requests.post(url=url, data={'source_path': source_path})
+try:
+    r = requests.post(url=url, data={'source_path': source_path})
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print(e)
 print(r.status_code)
 print(r.text)
 
@@ -34,6 +44,9 @@ print(r.text)
 print("Retrieve list of models")
 url = 'http://127.0.0.1:5011/v1/models'  # API endpoint
 # r = requests.get('https://api.github.com/user', auth=('user', 'pass')), because we need authentication for this one
-r = requests.get(url=url)
+try:
+    r = requests.get(url=url)
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print(e)
 print(r.status_code)
 print(r.text)
